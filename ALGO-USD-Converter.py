2a318b2d1837b7e305bbee61e66e4ac3840e8d10
auxiliaryocelot <105829388+auxiliaryocelot@users.noreply.github.com>
2022-12-26 18:13:12 -0500

    Update ALGO-USD-Converter.py

--- ALGO-USD-Converter.py
+++ ALGO-USD-Converter.py
@@ -27,7 +27,7 @@ response = requests.get(url, params=parameters)
 if response.status_code == 200:
     data = response.json()
     conversion = round(algo_amount * float(data["algorand"]["usd"]), 2)
-    conversion_formatted = "{:,}".format(conversion) # Insert comma for thousands
+    conversion_formatted = "{:,.2f}".format(conversion) # Insert comma for thousands
     algo_amount_formatted = "{:,.2f}".format(algo_amount) # Insert comma for thousands, round to two decimal places
     print(f"{algo_amount_formatted} $ALGO is currently equal to ${conversion_formatted} USD.")
 
